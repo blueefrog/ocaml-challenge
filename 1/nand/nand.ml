f6384a5c38fed98dea3b7d6a7f3ed282ba3c6a14
blueefrog <chiaradidu04@gmail.com>
2024-10-11 12:12:38 +0200

    nand

--- 1/nand/nand.ml
+++ 1/nand/nand.ml
@@ -1,4 +1,19 @@
-let b1 = true;;
-let b2 = true;;
+let nand1(a:bool)(b:bool):
+bool=not(a&&b)
 
-let conj b1 b2 = not (b1 && b2);;
+let nand2 (a:bool)(b:bool):
+bool = 
+if a
+  then (* a is true *)
+    (if b
+      then (*b is true*) false
+    else true)
+else (* a is false*)
+  false
+
+
+let nand2 (a:bool)(b:bool):
+bool =
+  match (a,b) with
+  | (true, true) -> false
+  | _ -> true
