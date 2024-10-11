142c5d374e0e8643dfc47002d7ecbdc5da95fb67
blueefrog <chiaradidu04@gmail.com>
2024-10-11 12:20:28 +0200

    xor

--- 1/xor/xor.ml
+++ 1/xor/xor.ml
@@ -1,4 +1,17 @@
-let b1 = true;;
-let b2 = true;;
+let xor1(a:bool)(b:bool):
+bool= (a||b) && (not(a&&b))
 
-let conj b1 b2 = (b1 || b2) && (not (b1 && b2));;
+let xor2(a:bool)(b:bool):
+bool= 
+if a
+  then (*a is true*)
+    (if b (*b is true*)
+      then false
+    else true)
+  else (*a is false*)
+    (if b (*b is true*)
+      then true
+    else false)
+
+let xor2(a:bool)(b:bool):
+bool= 
